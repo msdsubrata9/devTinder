3f2b5aeb26bee2c6dad05a82882a443e4ae5cafd
Subrata Saha <subrata.saha@jungleworks.com>
2024-11-13 17:21:31 +0530

    FEAT: APIs creation

--- src/app.js
+++ src/app.js
@@ -3,13 +3,10 @@ const connectDB = require("./config/database");
 const app = express();
 const User  = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res, next) => {
-  const user = new User({
-    firstName: "Subrata",
-    lastName: "Saha",
-    emailId: "subrata.saha@gmail.com",
-    password: "Subrata@1808",
-  });
+  const user = new User(req.body);
 
   try{
     await user.save();
@@ -18,6 +15,56 @@ app.post("/signup", async (req, res, next) => {
     res.status(400).send("Error saving the user"+err.message);
   }
 });
+
+app.get("/user" ,async(req,res)=>{
+const userEmail = req.body.emailId;
+try{
+const users = await User.find({emailId:userEmail});
+if(users.length === 0){
+  res.status(404).send("User not found");
+}
+else{
+  res.send(users);
+}
+}catch(err){
+  res.status(404).send("Something went wrong");
+}
+})
+
+app.get("/feed" ,async(req,res)=>{
+  try{
+  const users = await User.find({});
+  if(users.length === 0){
+    res.status(404).send("User not found");
+  }
+  else{
+    res.send(users);
+  }
+  }catch(err){
+    res.status(404).send("Something went wrong");
+  }
+  })
+
+  app.delete("/user" ,async(req,res)=>{
+    const userId = req.body.userId;
+    try{
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User has been deleted successfully");
+    }catch(err){
+      res.status(404).send("Something went wrong");
+    }
+    })
+
+    app.patch("/user" ,async(req,res)=>{
+      const userId = req.body.userId;
+      const data = req.body;
+      try{
+      const user = await User.findByIdAndUpdate(userId,data);
+      res.send("User has been updated successfully");
+      }catch(err){
+        res.status(404).send("Something went wrong");
+      }
+      })
 
 connectDB()
   .then(() => {
