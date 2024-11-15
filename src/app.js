81d976b6cccce8515ce95516c4f532e821bab4ca
Subrata Saha <subrata.saha@jungleworks.com>
2024-11-15 17:31:34 +0530

    FEAT: login post API implementation and API validation

--- src/app.js
+++ src/app.js
@@ -2,76 +2,106 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User  = require("./models/user");
+const {validateSignupData} = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
 app.post("/signup", async (req, res, next) => {
-  const user = new User(req.body);
+  try {
+    // validate signup data
+    validateSignupData(req);
+    const { firstName, lastName, emailId, password } = req.body;
+    // encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
 
-  try{
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
     await user.save();
     res.send("User added successfully");
-  } catch(err){
-    res.status(400).send("Error saving the user"+err.message);
+  } catch (err) {
+    res.status(400).send("Error saving the user" + err.message);
   }
 });
 
-app.get("/user" ,async(req,res)=>{
-const userEmail = req.body.emailId;
-try{
-const users = await User.find({emailId:userEmail});
-if(users.length === 0){
-  res.status(404).send("User not found");
-}
-else{
-  res.send(users);
-}
-}catch(err){
-  res.status(404).send("Something went wrong");
-}
-})
-
-app.get("/feed" ,async(req,res)=>{
-  try{
-  const users = await User.find({});
-  if(users.length === 0){
-    res.status(404).send("User not found");
-  }
-  else{
-    res.send(users);
-  }
-  }catch(err){
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
     res.status(404).send("Something went wrong");
   }
-  })
+});
 
-  app.delete("/user" ,async(req,res)=>{
-    const userId = req.body.userId;
-    try{
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(404).send("Something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
     const user = await User.findByIdAndDelete(userId);
     res.send("User has been deleted successfully");
-    }catch(err){
-      res.status(404).send("Something went wrong");
-    }
-    })
+  } catch (err) {
+    res.status(404).send("Something went wrong");
+  }
+});
 
-    app.patch("/user/:userId" ,async(req,res)=>{
-      const userId = req.params?.userId;
-      const data = req.body;
-      try{
-      const ALLOWED_UPDATES = ["about","gender","skills","age"];
-      const isUpdateAllowed = Object.keys(data).every((k)=>{
-        ALLOWED_UPDATES.includes(k);
-      })
-      if(!isUpdateAllowed){
-        throw new Error("Update not allowed");
-      }
-      const user = await User.findByIdAndUpdate(userId,data,{runValidators:true});
-      res.send("User has been updated successfully");
-      }catch(err){
-        res.status(404).send("UPDATE FAILED: "+err.message);
-      }
-      })
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+  try {
+    const ALLOWED_UPDATES = ["about", "gender", "skills", "age"];
+    const isUpdateAllowed = Object.keys(data).every((k) => {
+      ALLOWED_UPDATES.includes(k);
+    });
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+    const user = await User.findByIdAndUpdate(userId, data, {
+      runValidators: true,
+    });
+    res.send("User has been updated successfully");
+  } catch (err) {
+    res.status(404).send("UPDATE FAILED: " + err.message);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid Creds");
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      res.send("Login Successful");
+    } else {
+      throw new Error("Invalid Creds");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
 
 connectDB()
   .then(() => {
