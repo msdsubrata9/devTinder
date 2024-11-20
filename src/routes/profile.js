d5511f4687431e254e1b306b57bf7ed595f9ee72
Subrata Saha <subrata.saha@jungleworks.com>
2024-11-20 23:24:57 +0530

    FEAT: Profile Edit API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileFields } = require("../utils/validation");
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileFields(req)) {
+      throw new Error("Invalid Edit request");
+    }
+    const loggedInUserData = req.user;
+    Object.keys(req.body).forEach((key) => {
+      loggedInUserData[key] = req.body[key];
+    });
+    await loggedInUserData.save();
+    res.json({
+      message: `${loggedInUserData.firstName}, your profile has been updated successfully`,
+      data: loggedInUserData,
+    });
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
